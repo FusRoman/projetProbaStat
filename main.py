2e488a9ded83bf9cdd103b4875444347c72bfd9d
FusRoman <roman.lemontagner@gmail.com>
2020-05-12 20:55:12 +0200

    V1 de l'algo, ne converge pas sur les données

--- main.py
+++ main.py
@@ -7,38 +7,66 @@ import matplotlib.pyplot as plt
 # T : tradeoff parameter // tradeoff between energy and entropy like terms
 
 
+
 def init_prob(nbData, nbCluster):
-    t = np.random.sample( (nbData, nbCluster) )
+    t = np.random.sample( (nbCluster, nbData) )
     return np.apply_along_axis(lambda a : np.divide(a, np.sum(a)), axis=1, arr=t)
 
-s = np.genfromtxt("data/DS1/simil_ds1.d")
-"""
-print(np.shape(s))
-
-print(np.shape(s[:, 0]))
-print(np.shape(s[:, 1]))
-print()
-print(s[:10,0])
-print()
-print(s[:10, 1])
-"""
-
-
-s = np.loadtxt("data/DS1/scatter_ds1.d")
-print(np.shape(s))
-
-
-
-def gen_cluster(similarity_matrix, T, nbCluster, epsilon):
+def gen_cluster(similarity_matrix, Nc, T=2, epsilon=0.0001):
     nbData = np.shape(similarity_matrix)[0]
-    proba_cluster = init_prob(nbData, nbCluster)
-    
+    Pci = init_prob(nbData, Nc)
+
     while True:
+        lastPci = np.copy(Pci)
+        
         for i in range(nbData):
-            for N in range(nbCluster):
-                
+            Pi = 1/5
+            
+            Pc = np.sum(Pci, axis = 1) * Pi
+            
+            sCi = np.multiply(np.sum(np.multiply(Pci, similarity_matrix[:, i]), axis = 1), np.divide(Pc, Pi))
+            
+            
+            #zone non optimisé du cul
+            #calcul de la double somme s(C)
+            sC = np.empty(Nc)
+            for C in range(Nc):
+                total = 0
+                for k in range(nbData):
+                    tmpTot = 0
+                    for l in range(nbData):
+                        factor = Pi / Pc[C]
+                        tmpTot += (Pci[C,k] * factor) * (Pci[C,l] * factor) * similarity_matrix[k,l]
+                    total += tmpTot
+                sC[C] = total
+            #fin de la zone non optimisé du cul
+            
+            newPci = np.multiply(np.exp(np.divide(np.subtract(np.multiply(sCi, 2), sC), 1/T)), Pc)
+            
+            Pci[:, i] = newPci
+            
+            
+            
+            newPci = np.divide(Pci[:,i], np.sum(Pci[:, i]))
+            
+            Pci[:, i] = newPci
+            
+        if np.all(np.less_equal(np.abs(np.subtract(Pci, lastPci)), epsilon)):
+                break
+
+    return Pci
 
 
+
+s = np.genfromtxt("data/DS1/simil_ds1.d")
+
+#bug : l'algo ne converge pas
+#bug 2 : warning python ligne 50 -> RuntimeWarning: invalid value encountered in true_divide
+res = gen_cluster(s, 3)                
+
+for i in range(3):
+    print(res[:, i])
+    print()
 
 #show result data
 """
